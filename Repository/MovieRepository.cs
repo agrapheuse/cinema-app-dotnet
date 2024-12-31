11fa608e580afeb35edbfaeb5d54139f94379644
agrapheuse <noah.diderich@gmail.com>
2024-12-31 12:07:23 +0100

    eager loading cinema

--- Repository/MovieRepository.cs
+++ Repository/MovieRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
@@ -10,9 +11,11 @@ public class MovieRepository : RepositoryBase<Movie>, IMovieRepository
     }
 
     public IEnumerable<Movie> GetAllMovies(bool trackChanges) =>
-        FindAll(trackChanges);
+        FindAll(trackChanges).Include(movie => movie.Cinema);
     public IEnumerable<Movie> GetMoviesForCity(string city, bool trackChanges) =>
-        FindByCondition(movie => movie.Cinema.City == city, trackChanges).OrderBy(movie => movie.DateTime);
+        FindByCondition(movie => movie.Cinema.City == city, trackChanges)
+            .Include(movie => movie.Cinema)
+            .OrderBy(movie => movie.DateTime);
 
     public Movie GetMovieById(Guid guid, bool trackChanges) =>
         FindByCondition(movie => movie.Uuid == guid, trackChanges).FirstOrDefault();
